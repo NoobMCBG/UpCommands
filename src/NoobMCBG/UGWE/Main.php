5dbc85514a48afb9660e05d6ba210269722410e4
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-05-27 16:15:46 +0700

    Update Main.php

--- src/NoobMCBG/UGWE/Main.php
+++ src/NoobMCBG/UGWE/Main.php
@@ -2,6 +2,7 @@
 
 namespace NoobMCBG\UGWE;
 
+use pocketmine\player\Player;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
@@ -17,26 +18,26 @@ class Main extends PluginBase {
 		switch($cmd->getName()){
 			case "/up":
 			    	if(!$sender instanceof Player){
-			    		if(isset($args[0])){
-			    	    		if(!is_numeric($args[0])){
-                            				$sender->sendMessage("§cUsage: //up <y>");
-                           				return true;
-                        			}
-						$x = $sender->getPosition()->getX();
-						$y = $sender->getPosition()->getY();
-						$z = $sender->getPosition()->getZ();
-						$world = $sender->getPosition()->getWorld();
-						if($y + (int)$args[0] < 255){
-							$world->setblock(new Vector3($x, $y+$args[0], $z), BlockFactory::getInstace()->get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
-						}else{
-							$sender->sendMessage("§cY coordinate should not be more than 255");
-						}
-				    	}else{
-				    		$sender->sendMessage("§cUsage: //up <y>");
-				    	}
-			    	}else{
-			    		$sender->sendMessage("§cUse Command in-game");
-			    	}
+					$sender->sendMessage("§cUse Command in-game");
+					return true;
+				}
+				if(isset($args[0])){
+					if(!is_numeric($args[0])){
+						$sender->sendMessage("§cUsage: //up <y>");
+						return true;
+					}
+					$x = $sender->getPosition()->getX();
+					$y = $sender->getPosition()->getY();
+					$z = $sender->getPosition()->getZ();
+					$world = $sender->getPosition()->getWorld();
+					if($y + (int)$args[0] < 255){
+						$world->setblock(new Vector3($x, $y+$args[0], $z), BlockFactory::getInstace()->get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
+					}else{
+						$sender->sendMessage("§cY coordinate should not be more than 255");
+					}
+				}else{
+					$sender->sendMessage("§cUsage: //up <y>");
+				}
 			break;
 		}
 		return true;
