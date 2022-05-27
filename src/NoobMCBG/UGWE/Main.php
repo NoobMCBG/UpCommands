73402c6a6a2cb84929fe0644ecc68bd982c92605
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-05-27 15:55:04 +0700

    Update Main.php

--- src/NoobMCBG/UGWE/Main.php
+++ src/NoobMCBG/UGWE/Main.php
@@ -2,10 +2,7 @@
 
 namespace NoobMCBG\UGWE;
 
-use pocketmine\item\Item;
-use pocketmine\block\Block;
 use pocketmine\command\CommandSender;
-use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 use pocketmine\math\Vector3;
@@ -19,23 +16,26 @@ class Main extends PluginBase {
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		switch($cmd->getName()){
 			case "/up":
-			    if(!$sender instanceof Player){
-			    	if(is_numeric($args[0])){
-			    	    if(!isset($args[0])){
-                            $sender->sendMessage("§cUsage: //up <y>");
-                            return true;
-                        }
-				        $x = $sender->getX();
-				        $y = $sender->getY();
-				        $z = $sender->getZ();
-				        $level = $sender->getLevel();
-				        $level->setblock(new Vector3($x, $y+$args[0], $z), Block::get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
-				    }else{
-				    	$sender->sendMessage("§cUsage: //up <y>");
-				    }
-			    }else{
-			    	$sender->sendMessage("Use Command in-game");
-			    }
+			    	if(!$sender instanceof Player){
+			    		if(isset($args[0])){
+			    	    		if(!is_numeric($args[0])){
+                            				$sender->sendMessage("§cUsage: //up <y>");
+                           				return true;
+                        			}
+						$x = $sender->getPosition()->getX();
+						$y = $sender->getPosition()->getY();
+						$z = $sender->getPosition()->getZ();
+						$world = $sender->getPosition()->getLevel();
+						if($y + (int)$args[0] < 255){
+							$world->setblock(new Vector3($x, $y+$args[0], $z), BlockFactory::getInstace()->get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
+						}else{
+							$sender->sendMessage("§cY coordinate should not be more than 255");
+				    	}else{
+				    		$sender->sendMessage("§cUsage: //up <y>");
+				    	}
+			    	}else{
+			    		$sender->sendMessage("§cUse Command in-game");
+			    	}
 			break;
 		}
 		return true;
