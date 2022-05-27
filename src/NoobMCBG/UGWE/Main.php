b2b94300a4913347b6ded71ed6386e671443a22d
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-05-27 16:24:15 +0700

    Update Main.php

--- src/NoobMCBG/UGWE/Main.php
+++ src/NoobMCBG/UGWE/Main.php
@@ -16,27 +16,27 @@ class Main extends PluginBase {
 
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		if($cmd->getName() == "/up"){
-			    if(!$sender instanceof Player){
+			if(!$sender instanceof Player){
 				$sender->sendMessage("§cUse Command in-game");
 				return true;
 			}
 			if(isset($args[0])){
-				if(!is_numeric($args[0])){
-					$sender->sendMessage("§cUsage: //up <y>");
-					return true;
-				}
-				$x = $sender->getPosition()->getX();
-				$y = $sender->getPosition()->getY();
-				$z = $sender->getPosition()->getZ();
-				$world = $sender->getPosition()->getWorld();
-				if($y + (int)$args[0] < 255){
-					$sender->sendMessage("§cY coordinate should not be more than 255");
-					return true;
-				}
-					$world->setblock(new Vector3($x, $y+$args[0], $z), BlockFactory::getInstace()->get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
-			}else{
 				$sender->sendMessage("§cUsage: //up <y>");
+				return true;
 			}
+			if(!is_numeric($args[0])){
+				$sender->sendMessage("§cUsage: //up <y>");
+				return true;
+			}
+			$x = $sender->getPosition()->getX();
+			$y = $sender->getPosition()->getY();
+			$z = $sender->getPosition()->getZ();
+			$world = $sender->getPosition()->getWorld();
+			if($y + (int)$args[0] < 255){
+				$sender->sendMessage("§cY coordinate should not be more than 255");
+				return true;
+			}
+			$world->setblock(new Vector3($x, $y+$args[0], $z), BlockFactory::getInstace()->get($this->getConfig()->get("id-block"), $this->getConfig()->get("meta-block")));
 		}
 		return true;
 	}
